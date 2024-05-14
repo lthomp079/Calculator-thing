a175709a3136725f3ce6a1adbb645ff8dcde084c
lthomp079 <163169113+lthomp079@users.noreply.github.com>
2024-05-14 16:22:01 -0400

    Add files via upload

---
+++ ihavegcc.bat
@@ -0,0 +1,7 @@
+@echo off
+cd ..
+cd src
+g++ -o calc.exe ianisnotgay.cpp
+echo Go Back, and the to the source folder. run calc.exe now to run me. 
+echo You can move the executable as you wish.
+.\calc.exe
---
+++ ihavepython.bat
@@ -0,0 +1,5 @@
+@echo off
+cd .. 
+cd src
+python ianisnotgay.py
+echo Run this file again assuming you still have python to run the calc
